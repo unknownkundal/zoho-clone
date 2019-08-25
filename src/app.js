03a1ec4a90bc492ea291a5be8cdf318241e2877e
hitesh chauhan <hiteshchauhan.669@gmail.com>
2019-08-25 12:23:47 +0530

    production ready

---
+++ src/app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const publicPath = path.join(__dirname, '../public');
+console.log(publicPath);
+app.use(express.static(publicPath));
+const port = process.env.PORT || 3000;
+app.listen(port);
